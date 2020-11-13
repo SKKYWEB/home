7bfab1cee9bb265360acc849492778f2ae444a05
SKKYWEB <74390833+SKKYWEB@users.noreply.github.com>
2020-11-13 10:55:22 +0800

    origin

---
+++ precache-manifest.106f13b1edf65a94a19d21d56f7ba7b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3155168e37ba4de00c2a8e64105d7d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e587d6a0405691e0366",
+    "url": "./static/css/main.0d52b1bd.chunk.css"
+  },
+  {
+    "revision": "c106f86e3fe2483f7e1f",
+    "url": "./static/js/2.4c1a3db6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4c1a3db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e587d6a0405691e0366",
+    "url": "./static/js/main.297148bf.chunk.js"
+  },
+  {
+    "revision": "1dd629fba31da3fc5a61",
+    "url": "./static/js/runtime-main.eb12ce3a.js"
+  },
+  {
+    "revision": "1ca76bab9c0b9dda826ddc82be582eba",
+    "url": "./static/media/home-day.1ca76bab.jpg"
+  },
+  {
+    "revision": "b4102a39cd96fd3bad2a3ca8ce11126f",
+    "url": "./static/media/lond.b4102a39.png"
+  },
+  {
+    "revision": "718e5325f891b3656a15382c1ebbd9b4",
+    "url": "./static/media/profile.718e5325.png"
+  },
+  {
+    "revision": "3501dd5d32f2e5b11d7b7fc1480dda63",
+    "url": "./static/media/service.3501dd5d.png"
+  }
+]);
